05cd34194778e2d0965833cdb2a9eb2128ff946c
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2024-12-27 22:01:10 +0530

    File System Module

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const fs = require('fs');
 
+// Blocking , synchronous way
+const data = fs.readFileSync('./file/text.txt' , 'utf-8');
 const hello = "Hello Kanhaiya!";
-console.log(hello)
+
+const textOut = 'This is what we know about the avocado';
+fs.writeFileSync('./file/output.txt' , textOut)
+console.log(hello , data)
+
+// Non-blocking, asynchronous way
+
+fs.readFile('./file/output.txt' , 'utf-8' , (err , data) => {
+    if(err) {
+        throw err
+    } else {
+        console
+        .log(data , "Check Data")
+    }
+})
+
+fs.writeFile('./file/new.txt' , 'create new file and write something' , 'utf-8' , (err) => {
+    if(err) return console.log('ERROR!')
+})
