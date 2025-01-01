8f1a08c78be114d94d83f031bac007bd981c6537
jayaskanhaiya0001 <jayaskanhaiya0001@gmail.com>
2025-01-01 11:12:21 +0530

    Delete Node Modules

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
-
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate')
 // Blocking , synchronous way
 // const data = fs.readFileSync('./file/text.txt' , 'utf-8');
 // const hello = "Hello Kanhaiya!";
@@ -30,19 +30,6 @@ const url = require('url');
 
 
 // SERVER
-const replaceTemplate = (temp , product) => {
-let output = temp.replace(/{%PRODCTNAME%}/g , product.productName);
-output = output.replace(/{%IMAGE%}/g , product.image);
-output = output.replace(/{%PRICE%}/g , product.price);
-output = output.replace(/{%FROM%}/g , product.from);
-output = output.replace(/{%NUTRIENTS%}/g , product.nutrients);
-output = output.replace(/{%QUANTITY%}/g , product.quantity);
-output = output.replace(/{%DESCRIPTION%}/g , product.description);
-output = output.replace(/{%ID%}/g , product.id);
-output = output.replace(/{%IMAGE%}/g , product.image);
-if(product.organic) output = output.replace(/{%NOT_ORGANIC%}/ , 'not-organic')
-return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html` , 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html` , 'utf-8');
@@ -50,11 +37,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html` , 'utf
 // console.log(tempOverview , tempCard ,tempProduct , 'Check This Line' )
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName , {lower: true}));
+console.log(slugs , 'slugs')
 const server = http.createServer((req, res) => {
     const {query , pathname} = url.parse(req.url , true);
-
-    const pathName = req.url;
-
     //Overview Page
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {
